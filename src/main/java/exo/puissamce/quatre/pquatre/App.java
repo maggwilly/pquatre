06408cf1eadd9f991302916b69fc3cf98b3c3ab8
maggwilly <williams.penka@gmail.com>
2020-04-24 17:49:21 +0400

    Initialisation du projet Puissance Quatre

---
+++ src/main/java/exo/puissamce/quatre/pquatre/App.java
@@ -0,0 +1,13 @@
+package exo.puissamce.quatre.pquatre;
+
+/**
+ * Lancement de l'application!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
